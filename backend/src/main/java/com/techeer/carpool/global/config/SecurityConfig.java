b4400f41a18424feea2237348c6949ffb7227ef8
Sua <140071879+ssuasu@users.noreply.github.com>
2026-05-01 21:58:25 +0900

    fix : 실행을 위한 수정

--- backend/src/main/java/com/techeer/carpool/global/config/SecurityConfig.java
+++ backend/src/main/java/com/techeer/carpool/global/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
